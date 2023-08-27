389c9254648257028069d5d90d400b144b3415b7
ManuelLG92 <lavacars.ecologic@gmail.com>
2023-08-27 19:45:29 +0200

    feat: renaming

--- src/controllers/post/postController.ts
+++ src/controllers/post/postController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
 import { ValidateBody } from '../validators/requestValidator';
-import { testSchema } from './testValidator';
+import { postDtoSchema } from './postDtoSchema';
 import { BaseController } from '../baseController';
 
 export class PostController extends BaseController {
-	@ValidateBody(testSchema)
+	@ValidateBody(postDtoSchema)
 	async execute(req: Request, res: Response) {
-		const { body: dto }: { body: Zod.infer<typeof testSchema> } = req;
+		const { body: dto }: { body: Zod.infer<typeof postDtoSchema> } = req;
 		console.log('dto', dto);
 		return res.status(200).json({ dto });
 	}
--- src/controllers/post/postDtoSchema.ts
+++ src/controllers/post/postDtoSchema.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 
-export const testSchema = z
+export const postDtoSchema = z
 	.object({
 		username: z.string().min(3).max(30),
 		age: z.number().min(18).max(65),
