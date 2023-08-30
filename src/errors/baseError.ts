c50fe003dc9b3188dc1b47a3dcea95459976d862
ManuelLG92 <lavacars.ecologic@gmail.com>
2023-08-31 00:01:15 +0200

    fix: optional message

--- src/errors/baseError.ts
+++ src/errors/baseError.ts
@@ -1,4 +1,9 @@
 export abstract class BaseError extends Error {
-	abstract readonly message: string;
+	readonly message: string;
 	abstract readonly details: Array<Record<string, unknown>>;
+
+	constructor(message?: string){
+		super()
+		this.message = message ?? this.constructor.name
+	}
 }
--- src/errors/unprocessableEntity.ts
+++ src/errors/unprocessableEntity.ts
@@ -1,11 +1,9 @@
 import { BaseError } from './baseError';
 
 export class UnprocessableEntity extends BaseError {
-	message: string;
 	details: Record<string, unknown>[];
 	constructor(details: Record<string, unknown>[]) {
 		super();
-		this.message = this.constructor.name;
 		this.details = details;
 	}
 }
